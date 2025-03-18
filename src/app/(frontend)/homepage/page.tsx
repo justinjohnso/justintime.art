2f4bc80775fce0c976de334d0e21e10f0d89cb37
Justin Johnson <justinaugustejohnson@gmail.com>
2025-03-18 18:15:45 -0400

    more projects setup

--- src/app/(frontend)/homepage/page.tsx
+++ src/app/(frontend)/homepage/page.tsx
@@ -1,7 +1,7 @@
-import { GetStaticProps } from 'next';
-import { useEffect, useState } from 'react';
-import payload from 'payload';
-import { Project } from '@/payload-types';
+import { GetStaticProps } from 'next'
+import { useEffect, useState } from 'react'
+import payload from 'payload'
+import { Project } from '@/payload-types'
 
 const HomePage = ({ projects }: { projects: Project[] }) => {
   return (
@@ -13,19 +13,19 @@ const HomePage = ({ projects }: { projects: Project[] }) => {
         ))}
       </ul>
     </div>
-  );
-};
+  )
+}
 
 export const getStaticProps: GetStaticProps = async () => {
   const projects = await payload.find({
     collection: 'projects',
-  });
+  })
 
   return {
     props: {
       projects: projects.docs,
     },
-  };
-};
+  }
+}
 
-export default HomePage;
+export default HomePage
